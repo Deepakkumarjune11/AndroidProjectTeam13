4c0e58f77282377c31059a6aaf50aa5feed81cbe
Deepakkumar S <dpkkumarjun7@Outlook.com>
2021-12-13 11:26:33 +0530

    Hi

--- app/src/main/java/com/example/myprojpage1/Register.kt
+++ app/src/main/java/com/example/myprojpage1/Register.kt
@@ -1,13 +1,21 @@
 package com.example.myprojpage1
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.Window
+import android.widget.Button
 
 class Register : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
         getSupportActionBar()?.hide()
+        var btnreg=findViewById<Button>(R.id.regbtn1)
+        val log= Intent(this,Loginpage::class.java)
+
+        btnreg.setOnClickListener{
+            startActivity(log)
+        }
     }
 }
